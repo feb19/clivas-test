9fae03b3eb26f54246f0720494633ec18b1ffc34
Nobuhiro Takahashi <nt@feb19.jp>
2013-11-30 12:37:02 +0900

    drawing test

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 var clivas = require('clivas');
+var paint = require('paint');
 
 var frame = 0;
+var width = 0;
 
-setInterval(function() {
-    clivas.clear(); // clears the canvas
+var draw = function() {
+    clivas.clear();
     clivas.line('hello world (#frame '+frame+')');
     clivas.line('{red:also} {green:colors}!');
+    clivas.write('[7m[32m[39m');
     frame++;
-}, 200);
+    
+    clivas.line('{red+inverse:           }');
+
+    width++;
+    if (width > 20) return clivas.line('({red:boom!})');
+    clivas.line('[{20+green:'+Array(width).join('=')+'>}]');
+};
+
+setInterval(draw, 200);
